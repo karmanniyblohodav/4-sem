2138119f80771af3969a8ba7f8139a556c7ee257
karmanniyblohodav <124428373+karmanniyblohodav@users.noreply.github.com>
2025-04-04 20:58:23 +0300

    Задание 1

---
+++ Task 1/Task 1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+
+int main() {
+    std::string filename = "name.txt";
+    std::ifstream file(filename);
+
+    if (!file) {
+        std::cerr << "Ошибка открытия файла!" << std::endl;
+        return 1;
+    }
+
+    std::istream_iterator<int> start(file), end;
+
+    std::ostream_iterator<int> output(std::cout, " ");
+
+    std::copy(start, end, output);
+    std::cout << std::endl;
+
+    return 0;
+}
